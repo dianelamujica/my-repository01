e96e28531410d15dc809059c471f1087a492de31
dianelamujica <dianela.mujica@gmail.com>
2026-06-30 22:11:44 +0200

    Add files via upload

---
+++ Streamlit_file.py
@@ -0,0 +1,9 @@
+
+import warnings
+import sys
+import streamlit as st
+
+st.header ("Mein erste Streamlit Projekt")
+st.subheader ("Eine Unter-Überschrift")
+st.write ("Hallo Welt!")
+st.write("Streamlit ist gar nicht schwerz")
